55f9bf2a972ddf8a57c16e30da5c73e5cc1326c5
juliaers <juliaemilyreis2003@gmail.com>
2026-01-14 16:54:13 -0300

    Subindo models

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#Tabela no banco de dados do Django = Cadastro de pacientes
+
+class Paciente(models.Model):
+    nome_completo = models.DateField(max_length=200)
+    data_nascimento = models.DateField()
+    diagnostico_principal = models.TextField()
+    familiar_responsavel = models.CharField(max_length=200)
+    contato_familiar = models.CharField(max_length=50)
+    observacoes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nome_completo
