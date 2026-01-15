ac8af5d726b23ab9bbce439760f1a9304b0de3c6
juliaers <juliaemilyreis2003@gmail.com>
2026-01-15 10:54:57 -0300

    Criando views

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from .forms import PacienteForm
 
-# Create your views here.
+def cadastrar_paciente(request):
+    if request.method == 'POST':
+        form = PacienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sucesso')
+    else:
+        form = PacienteForm()
+    
+    return render(request, 'pacientes/cadastro.html',
+    {'form':form})
