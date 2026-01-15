6c0021a118380da0f3499b84ddeee34b0886e81d
Newton Filho <jnewton.fff@gmail.com>
2026-01-15 17:58:41 -0300

    Cria forms.py

--- cuidador/views.py
+++ cuidador/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Cuidador
+from .forms import CuidadorForm
 
-# Create your views here.
+def listar_pacientes_ativos(request):
+    pacientes = Paciente.objects.filter(status='ativo')
+    return render(request, 'paciente/listar_pacientes_ativos.html'), ('pacientes': pacientes)
+
+def cadastrar_paciente(request):
+    if request.method == 'POST':
+        form = PacienteForm(request.POST)
