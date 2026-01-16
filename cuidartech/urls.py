c4fa97ad63d77c042e34217a5861dcdb6e4ddd5b
juliaers <juliaemilyreis2003@gmail.com>
2026-01-16 10:33:57 -0300

    Arrumando erro do nome completo

--- cuidartech/urls.py
+++ cuidartech/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pacientes/', include('pacientes.urls')),
 ]
 
-path('pacientes/', include('pacientes.urls')),
+
