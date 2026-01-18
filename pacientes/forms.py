0fefae7d8c00de3608f5e23a8029296b4f4614ad
juliaers <juliaemilyreis2003@gmail.com>
2026-01-17 23:42:46 -0300

    Ajeitando layout

--- pacientes/forms.py
+++ pacientes/forms.py
@@ -3,14 +3,33 @@ from .models import Paciente
 
 #Criando formulario
 
+from django import forms
+from .models import Paciente
+
 class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
-        fields = [
-            'nome_completo',
-            'data_nascimento',
-            'diagnostico_principal',
-            'familiar_responsavel',
-            'contato_familiar',
-            'observacoes',
-        ]
+        fields = '__all__'
+        widgets = {
+            'nome_completo': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'data_nascimento': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date'
+            }),
+            'diagnostico_principal': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3
+            }),
+            'familiar_responsavel': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'contato_familiar': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'observacoes': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3
+            }),
+        }
