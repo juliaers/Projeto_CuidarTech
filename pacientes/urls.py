42650d2f7492c2ae8848fa1ec0f062a142453347
juliaers <juliaemilyreis2003@gmail.com>
2026-01-17 22:41:19 -0300

    Criando html

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from.views import cadastrar_paciente
 
 urlpatterns = [
-    path('cadastrar/', cadastrar_paciente,
-name='cadastrar_paciente'),
+    path('cadastrar/', cadastrar_paciente, name='cadastrar_paciente'),
 ]
