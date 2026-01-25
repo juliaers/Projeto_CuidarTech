d3f4bf1858b5f970df570505c8817dd52c2f9f11
Newton Filho <jnewton.fff@gmail.com>
2026-01-25 15:50:57 -0300

    Configura autenticação, perfil profissional e signals

---
+++ profissionais/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class ProfissionaisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'profissionais'
+
+    def ready(self):
+        import profissionais.signals
---
+++ profissionais/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+from django.db import models
+# Definição de User
+User = get_user_model()
+
+
+class ContextoAtuacao(models.TextChoices):
+    HOSPITALAR = "HOSP", "Hospitalar"
+    DOMICILIAR = "DOM", "Domiciliar"
+    CLINICA = "CLI", "Clinica"
+
+class Profissional(models.Model):
+    CATEGORIA = [
+        ('cuidador', 'Cuidador(a)'),
+        ('enfermeiro', 'Enfermeiro(a)')
+    ]
+    
+    # Enxugar para um primeiro momento
+    CONSELHOS_SAUDE = [
+    ("CRBM", "CRBM - Biomedicina"),
+    ("CRM", "CRM - Medicina"),
+    ("COREN", "COREN - Enfermagem"),
+    ("CRO", "CRO - Odontologia"),
+    ("CRF", "CRF - Farmácia"),
+    ("CRP", "CRP - Psicologia"),
+    ("CREFITO", "CREFITO - Fisioterapia / Terapia Ocupacional"),
+    ("CRBio", "CRBio - Biologia"),
+    ("CREFONO", "CREFONO - Fonoaudiologia"),
+    ("CRN", "CRN - Nutrição"),
+    ("CRESS", "CRESS - Serviço Social"),
+    ("CREF", "CREF - Educação Física"),
+    ("CRQ", "CRQ - Química"),
+    ("OUTRO", "Outro / Não se aplica"),
+]
+    STATUS = [
+        ("ativo", "Ativo"),
+        ("inativo", "Inativo")
+    ]
+
+    id = models.BigAutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=150),
+    categoria = models.CharField(max_length=20, choices=CATEGORIA)
+    conselho = models.CharField(max_length=40, choices=CONSELHOS_SAUDE)
+    numero_conselho = models.CharField(max_length=30)
+    status = models
+    # Define o contexto de atuação como "Domiciliar", sem poder editar
+    ContextoAtuacao = models.CharField(max_length=5, default="DOM", editable=False)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}".strip() - {self.categoria} or self.user.username 
+
+    
