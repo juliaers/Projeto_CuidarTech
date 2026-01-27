79940a6ea569ff7e6567b6e882de9de4ad542d8c
juliaers <juliaemilyreis2003@gmail.com>
2026-01-27 07:11:36 -0300

    Importando phonenumbers

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 #Tabela no banco de dados do Django = Cadastro de pacientes
 
 class Paciente(models.Model):
@@ -7,7 +7,7 @@ class Paciente(models.Model):
     data_nascimento = models.DateField()
     diagnostico_principal = models.TextField()
     familiar_responsavel = models.CharField(max_length=100)
-    contato_familiar = models.CharField(max_length=50)
+    contato_familiar = PhoneNumberField(blank=True)
     observacoes = models.TextField(blank=True)
 
     def __str__(self):
